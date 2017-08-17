6a09db0e45c15851924ffa0490750d318f7a097f
Tony Su <peitaosu@163.com>
2017-08-17 20:54:21 +0800

    add a SoundsRecord for Windows platform

---
+++ SoundsRecord/win32/SoundsRecord/SoundsRecord/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Some Code Fragments from:
+   1. https://stackoverflow.com/questions/18812224/c-sharp-recording-audio-from-soundcard - Florian R.
+ */
+
+using System;
+using CSCore.SoundIn;
+using CSCore.Codecs.WAV;
+
+namespace SoundsRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WasapiCapture capture = new WasapiLoopbackCapture())
+            {
+
+                //initialize the selected device for recording
+                capture.Initialize();
+
+                //create a wavewriter to write the data to
+                using (WaveWriter w = new WaveWriter("dump.wav", capture.WaveFormat))
+                {
+                    //setup an eventhandler to receive the recorded data
+                    capture.DataAvailable += (s, e) =>
+                    {
+                        //save the recorded audio
+                        w.Write(e.Data, e.Offset, e.ByteCount);
+                    };
+
+                    //start recording
+                    capture.Start();
+
+                    Console.ReadKey();
+
+                    //stop recording
+                    capture.Stop();
+                }
+            }
+        }
+    }
+}
