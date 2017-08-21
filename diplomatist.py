408879422f58150f54d3ff9fa2ebc43843fcf78d
Tony Su <peitaosu@163.com>
2017-08-21 22:09:09 +0800

    use LoopbackCapture in diplomatist.py

--- diplomatist.py
+++ diplomatist.py
@@ -1,6 +1,7 @@
-import os, sys
+import os, sys, time, subprocess
 import speech_recognition
 
+Loopback_Capture_Path = r"LoopbackCapture\win32\csharp\LoopbackCapture\LoopbackCapture\bin\Debug\LoopbackCapture.exe"
 
 class Diplomatist():
     def __init__(self):
@@ -31,12 +32,20 @@ class Diplomatist():
             audio = recognizer.listen(source)
         with open(audio_file, "wb") as f:
             f.write(audio.get_wav_data())
+    
+    def capture_loopback(self, audio_file, milliseconds):
+        process = subprocess.Popen("{} {} {}".format(Loopback_Capture_Path, audio_file, milliseconds), stdout=subprocess.PIPE)
+        exit_code = process.wait()
+        return exit_code
 
 if __name__ == "__main__":
     diplomatist = Diplomatist()
     audio_file = None
     if len(sys.argv) > 1:
         audio_file = sys.argv[1]
+    if len(sys.argv) > 2:
+        milliseconds = sys.argv[2]
+        diplomatist.capture_loopback(audio_file, milliseconds)
     result = diplomatist.transcribe(audio_file)
     if result:
         print result
