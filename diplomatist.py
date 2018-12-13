3b3311e8556efc6e453bc7c70cc28d879b6ac533
Tony Su <peitaosu@163.com>
2018-12-14 00:03:42 +0800

    move configurations to file and refined code

--- diplomatist.py
+++ diplomatist.py
@@ -6,6 +6,7 @@ import optparse
 import threading
 import platform
 import pydub
+import json
 
 import speech_recognition
 
@@ -19,20 +20,41 @@ elif platform.system() == "Windows":
 from MicRecord.MicRecord import record_mic
 
 class Diplomatist():
-    def __init__(self):
-        if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
+    def __init__(self, transcribe_api=0):
+        self.load_config()
+        self.set_transcribe_api(transcribe_api)
+        if self.config["API"]["1"]["cred"] != "":
             import google.cloud.translate
             self.translate_client = google.cloud.translate.Client()
-        if "OUT_SRT" in os.environ:
-            self.out = open(os.environ["OUT_SRT"], "a")
+        if self.config["STR"] != "":
+            self.str_out = open(self.config["STR"], "a")
+        for proxy in self.config["PROXY"]:
+            os.environ[proxy] = self.config["PROXY"][proxy]
 
-    def transcribe(self, api=0, audio_file=None, cred=None, language="en-US"):
+    def load_config(self):
+        with open("config.json") as in_file:
+            self.config = json.load(in_file)
+
+    def save_config(self):
+        with open("config.json", "w") as out_file:
+            json.dump(self.config, out_file, sort_keys=True, indent=4)
+
+    def set_transcribe_api(self, api=0):
+        self.api = api
+        self.cred = None
+        if "cred" in self.config["API"][str(api)] and self.config["API"][str(api)]["cred"] != "":
+            cred_config = self.config["API"][str(api)]["cred"]
+            if os.path.isfile(cred_config):
+                with open(cred_config) as in_file:
+                    self.cred = cred_config.read()
+            else:
+                self.cred = cred_config
+
+    def transcribe(self, audio_file=None, language="en-US"):
         """transcribe audio to text
 
         args:
-            api (0 - CMU Sphinx, 1 - Google Cloud, 2 - Bing API, 3 - Houndify API)
             audio_file (str)
-            cred (str)
             language (str)
 
         return:
@@ -48,19 +70,19 @@ class Diplomatist():
         with speech_recognition.AudioFile(audio_file) as source:
             audio = recognizer.record(source)
         try:
-            if api == 0:
+            if self.api == 0:
                 return recognizer.recognize_sphinx(audio, language)
-            elif api == 1:
-                return recognizer.recognize_google_cloud(audio, cred, language)
-            elif api == 2:
-                return recognizer.recognize_bing(audio, cred, language)
-            elif api == 3:
-                return recognizer.recognize_houndify(audio, cred.split(",")[0], cred.split(",")[1])
+            elif self.api == 1:
+                return recognizer.recognize_google_cloud(audio, self.cred, language)
+            elif self.api == 2:
+                return recognizer.recognize_bing(audio, self.cred, language)
+            elif self.api == 3:
+                return recognizer.recognize_houndify(audio, self.cred.split(",")[0], self.cred.split(",")[1])
         except speech_recognition.UnknownValueError:
-            print "Could Not Understand"
+            print("Could Not Understand")
             return False
         except speech_recognition.RequestError as e:
-            print "Request Error: {0}".format(e)
+            print("Request Error: {0}".format(e))
             return False
 
     def translate(self, text, language):
@@ -73,71 +95,66 @@ class Diplomatist():
         return:
             translated_text (str)
         """
-        if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
+        if hasattr(self, "translate_client"):
             return self.translate_client.translate(text, target_language=language)['translatedText']
 
-    def async_transcribe(self, api=0, audio_file=None, cred=None, language="en-US"):
+    def async_transcribe(self, audio_file=None, language="en-US"):
         """transcribe function for async running
 
         args:
-            api (0 - CMU Sphinx, 1 - Google Cloud, 2 - Bing API, 3 - Houndify API)
             audio_file (str)
-            cred (str)
             language (str)
         """
-        transc = self.transcribe(api, audio_file, cred, language)
+        transc = self.transcribe(audio_file, language)
         if transc == False:
             transc = "Could Not Be Transcribed!"
-        if hasattr(self, "out"):
-            self.out.write(transc + "\n")
-        print transc
+        if hasattr(self, "str_out"):
+            self.str_out.write(transc + "\n")
+        print(transc)
 
-    def async_transcribe_translate(self, api=0, audio_file=None, cred=None, transc_lan="en-US", transl_lan="zh"):
+    def async_transcribe_translate(self, audio_file=None, transc_lan="en-US", transl_lan="zh"):
         """transcribe with translate function for async running
 
         args:
-            api (0 - CMU Sphinx, 1 - Google Cloud, 2 - Bing API, 3 - Houndify API)
             audio_file (str)
-            cred (str)
             transc_lan (str)
             transl_lan (str)
         """
-        transc = self.transcribe(api, audio_file, cred, transc_lan)
+        transc = self.transcribe(audio_file, transc_lan)
         if transc == False:
             transc = "Could Not Be Transcribed!"
-        if hasattr(self, "out"):
-            self.out.write(transc + "\n")
-        print transc
+        if hasattr(self, "str_out"):
+            self.str_out.write(transc + "\n")
+        print(transc)
         transl = self.translate(transc, transl_lan)
-        if hasattr(self, "out"):
-            self.out.write(transl + "\n")
-        print transl
+        if hasattr(self, "str_out"):
+            self.str_out.write(transl + "\n")
+        print(transl)
 
-    def keep_running(self, cred, options):
+    def keep_running(self, options):
         """keep the process running until abort it
 
         args:
-            cred (str)
             options (OptionParser)
         """
         init_time = 0
         record_file = "record.wav"
-        records_folder = "records"
+        records_folder = self.config["RECORD"]
         if not os.path.isdir(records_folder):
             os.mkdir(records_folder)
         try:
             while True:
                 start_time = time.time()
                 if options.use_mic:
-                    self.record_mic(record_file, options.time_slice)
+                    record_mic(record_file, options.time_slice)
                 else:
-                    self.record_sounds(record_file, options.time_slice)
+                    record_sounds(record_file, options.time_slice)
                 end_time = time.time()
                 time_str = "{} --> {}".format(time.strftime("%H:%M:%S", time.gmtime(
                     init_time)), time.strftime("%H:%M:%S", time.gmtime(end_time - start_time + init_time)))
-                if hasattr(self, "out"):
-                    self.out.write(time_str + "\n")
-                print time_str
+                if hasattr(self, "str_out"):
+                    self.str_out.write(time_str + "\n")
+                print(time_str)
                 init_time = end_time - start_time + init_time
                 saved_file_name = str(time.time()) + ".wav"
                 saved_audio_file = os.path.join(
@@ -145,28 +162,27 @@ class Diplomatist():
                 os.rename(record_file, saved_audio_file)
                 if options.translate:
                     thr = threading.Thread(target=self.async_transcribe_translate, args=(
-                        [options.api, saved_audio_file, cred, options.language, options.translate]), kwargs={})
+                        [saved_audio_file, options.language, options.translate]), kwargs={})
                     thr.start()
                 else:
                     thr = threading.Thread(target=self.async_transcribe, args=(
-                        [options.api, saved_audio_file, cred, options.language]), kwargs={})
+                        [saved_audio_file, options.language]), kwargs={})
                     thr.start()
         except KeyboardInterrupt:
-            print "Process was exited."
+            print("Process was exited.")
 
-    def run_one_time(self, cred, options):
+    def run_one_time(self, options):
         """run the process one time
 
         args:
-            cred (str)
             options (OptionParser)
         """
         if options.translate:
             self.async_transcribe_translate(
-                options.api, options.audio_file, cred, options.language, options.translate)
+                options.audio_file, options.language, options.translate)
         else:
             self.async_transcribe(
-                options.api, options.audio_file, cred, options.language)
+                options.audio_file, options.language)
 
 
 def get_options():
@@ -181,16 +197,10 @@ def get_options():
                       help="time slice of each wave file")
     parser.add_option("-a", "--api", dest="api", default=0, type="int",
                       help="0 - CMU Sphinx, 1 - Google Cloud, 2 - Bing API, 3 - Houndify API")
-    parser.add_option("-c", "--cred", dest="credential", default=None,
-                      help="credential file if is API required")
     parser.add_option("-l", "--lan", dest="language", default="en-US",
                       help="language which to be transcribed")
     parser.add_option("-t", "--tran", dest="translate", default=None,
                       help="translate to another language")
-    parser.add_option("-o", "--out", dest="output", default=None,
-                      help="output the result as SRT file")
-    parser.add_option("-p", "--proxy", dest="proxy", default=None,
-                      help="set http/https proxy")    
     (options, args) = parser.parse_args()
     return options
 
@@ -201,27 +211,11 @@ if __name__ == "__main__":
             # change this location to your LoopbackCapture output executable file
             os.environ["LOOPBACK_CAPTURE"] = r"LoopbackCapture\win32\csharp\LoopbackCapture\LoopbackCapture\bin\Debug\LoopbackCapture.exe"
         if not os.path.isfile(os.environ["LOOPBACK_CAPTURE"]):
-            print "LOOPBACK_CAPTURE error: File Not Found"
+            print("LOOPBACK_CAPTURE error: File Not Found")
             sys.exit(-1)
     opt = get_options()
-    if opt.proxy:
-        os.environ["http_proxy"] = opt.proxy
-        os.environ["https_proxy"] = opt.proxy
-    if opt.credential:
-        if os.path.isfile(opt.credential):
-            cred = open(opt.credential, "r").read()
-        else:
-            cred = opt.credential
-        if opt.translate:
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = opt.credential
-        else:
-            cred = None
-    else:
-        cred = None
-    if opt.output:
-        os.environ["OUT_SRT"] = opt.output
     diplomatist = Diplomatist()
     if opt.audio_file:
-        diplomatist.run_one_time(cred, opt)
+        diplomatist.run_one_time(opt)
         sys.exit(0)
-    diplomatist.keep_running(cred, opt)
+    diplomatist.keep_running(opt)
