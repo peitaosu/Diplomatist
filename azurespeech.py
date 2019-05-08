55ad3fb0d3f0745fcfbce5ffee1ff1efe33740cf
Tony Su <peitaosu@163.com>
2019-05-07 21:59:36 -0700

    add class skeleton for azure speech

---
+++ azurespeech.py
@@ -0,0 +1,17 @@
+import sys, subprocess
+
+class AzureSpeechRecognizer():
+    def __init__(self):
+        pass
+
+    def recognize(self, audio_file):
+        """recognize audio file
+
+        args:
+            audio_file (str)
+
+        return:
+            result (str/False)
+        """
+        return None
+
