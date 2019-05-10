673b0b61060226eec5ca687da77a084a3252c9a5
Tony Su <peitaosu@163.com>
2019-05-09 22:23:23 -0700

    add azure speech config

--- azurespeech.py
+++ azurespeech.py
@@ -1,8 +1,12 @@
 import sys, subprocess
+import azure.cognitiveservices.speech as speechsdk
 
 class AzureSpeechRecognizer():
-    def __init__(self):
-        pass
+    def __init__(self, speech_key, service_region):
+        self.speech_key = speech_key
+        self.service_region = service_region
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.service_region)
+        self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config)
 
     def recognize(self, audio_file):
         """recognize audio file
